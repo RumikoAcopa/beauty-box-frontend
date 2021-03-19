30a7eb9aeacb96ee2396a86cc35299a5176e0ca1
rumikoacopa <rumikorios@gmail.com>
2021-03-19 00:06:56 -0400

    add Welcome Page

---
+++ src/components/WelcomePage.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class WelcomePage extends Component {
+  render() {
+    return (
+      <section>
+        <h1>Welcome To Your Beauty-Box-App</h1>
+      </section>
+    );
+  }
+}
