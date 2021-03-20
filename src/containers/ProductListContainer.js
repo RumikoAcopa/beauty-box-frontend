23b069c356e6e8b10fcd11f0353317eb58b0e041
rumikoacopa <rumikorios@gmail.com>
2021-03-20 02:46:32 -0400

    add import connect, fetch products action use props instead of state

--- src/containers/ProductListContainer.js
+++ src/containers/ProductListContainer.js
@@ -1,41 +1,45 @@
 import React, { Component } from "react";
+import { connect } from 'react-redux';
+import { fetchProducts } from '../actions/products';
+
 import ProductList from "../components/ProductList";
 
-export default class ProductListContainer extends Component {
-  state = {
-    groups: [],
-    loading: true
-  };
+class ProductListContainer extends Component {
+  
 
   componentDidMount() {
-    // we'd probably want to store the API_URL in an environment variable
-    // so this would work in deployment as well but for now we'll hard code the hostname
-    fetch("http://localhost:3001/products", {
-      method: "get",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
-    })
-      .then((res) => res.json())
-      .then((productsJson) => {
-        console.log("products", productsJson);
-        this.setState({
-          products: productsJson,
-          loading: false
-        });
-      });
+    this.props.fetchProducts;
   }
 
+
   render() {
+    if (this.props.loadingState ==='notStarted') {
+      return null;
+    }
     return (
-      <section className='max-w-6xl mx-auto mt-16'>
-        {this.state.loading ? (
+      <section className='max-w-6xlmx-auto   mt-16'>
+        {this.props.loadingState ==='inProgress' ? (
           "loading spinner"
         ) : (
-          <ProductListContainer products={this.state.groups} />
+          <ProductListContainer products={this.props.products} />
         )}
       </section>
     );
   }
+  
 }
+
+const mapStateToProps = (state) => {
+  return {
+    products: state.products.list,
+    loadingState: state.products.loadingState
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchProducts: () => dispatch(fetchProducts())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProductListContainer)
