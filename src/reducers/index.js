02d6dfdf3a0ab88a10917ec7d78a13e448dec63b
rumikoacopa <rumikorios@gmail.com>
2021-03-20 20:31:54 -0400

    add combine reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
 import { combineReducers } from "redux";
 import productsReducer from "./products"
-
+import categoryReducer from "./category"
 
 export default combineReducers({
-  products: productsReducer
+  products: productsReducer,
+  categories: categoryReducer
 });
