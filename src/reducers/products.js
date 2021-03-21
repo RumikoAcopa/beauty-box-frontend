e9aa6794038600934385723425fbb4d3f12f15ee
rumikoacopa <rumikorios@gmail.com>
2021-03-20 20:30:08 -0400

    add imports, add case w/rtn state

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,27 +1,40 @@
-import {ADD_PRODUCTS, START_LOADING_PRODUCTS, SUCCESS_LOADING_PRODUCTS, ERROR_LOADING_PRODUCTS} from "../actions"
+import {
+  ADD_PRODUCTS,
+  START_LOADING_PRODUCTS,
+  START_LOADING_PRODUCT,
+  SUCCESS_LOADING_PRODUCTS, ERROR_LOADING_PRODUCTS,
+  SUCCESS_LOADING_PRODUCT_CATEGORIES,} from "../actions"
 
 
 const defaultState = {
-  loadingState: 'notStarted',
+  loadingState: "notStarted",
   list: []
-}
+};
 
 export default function productsReducer(state = defaultState, action) {
   
   switch (action.type) {
-    
     case START_LOADING_PRODUCTS:
-      return { ...state, loadingState: 'inProgress' };
+      return { ...state, loadingState: "inProgress" };
+
     
     case SUCCESS_LOADING_PRODUCTS:
-      return { 
+      return {
         list: action.payload,
-        loadingState: 'successful'
-      }
-  
+        productsloadingState: "successful"
+      };
+
+    case SUCCESS_LOADING_PRODUCT_CATEGORIES:
+      const foundProduct = state.list.find(product => product.id == action.payload.product.id)
+
+      if (foundProduct) {
+        return state 
+        }else{
+        return {
+          ...state, list: state.list.concat(action.payload.product),
+        }
+      };
     default:
-      return state
-
+      return state;
   }
-
 }
