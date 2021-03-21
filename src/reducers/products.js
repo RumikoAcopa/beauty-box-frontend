d3bc8e9bde41773d5e63a11673c4a20f59762300
rumikoacopa <rumikorios@gmail.com>
2021-03-21 14:56:46 -0400

    import actions and modify if else statement

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -2,9 +2,10 @@ import {
   ADD_PRODUCTS,
   START_LOADING_PRODUCTS,
   START_LOADING_PRODUCT,
-  SUCCESS_LOADING_PRODUCTS, ERROR_LOADING_PRODUCTS,
-  SUCCESS_LOADING_PRODUCT_CATEGORIES,} from "../actions"
-
+  SUCCESS_LOADING_PRODUCTS,
+  ERROR_LOADING_PRODUCTS,
+  SUCCESS_LOADING_PRODUCT_CATEGORIES
+} from "../actions";
 
 const defaultState = {
   loadingState: "notStarted",
@@ -12,12 +13,10 @@ const defaultState = {
 };
 
 export default function productsReducer(state = defaultState, action) {
-  
   switch (action.type) {
     case START_LOADING_PRODUCTS:
       return { ...state, loadingState: "inProgress" };
 
-    
     case SUCCESS_LOADING_PRODUCTS:
       return {
         list: action.payload,
@@ -25,15 +24,19 @@ export default function productsReducer(state = defaultState, action) {
       };
 
     case SUCCESS_LOADING_PRODUCT_CATEGORIES:
-      const foundProduct = state.list.find(product => product.id == action.payload.product.id)
+      const foundProduct = state.list.find(
+        (product) => product.id == action.payload.product.id
+      );
 
       if (foundProduct) {
-        return state 
-        }else{
+        return state;
+      } else {
         return {
-          ...state, list: state.list.concat(action.payload.product),
-        }
-      };
+          ...state,
+          list: state.list.concat(action.payload.product)
+        };
+      }
+
     default:
       return state;
   }
