deac5ea67c42d28a5249235314091d05d0e63ee8
rumikoacopa <rumikorios@gmail.com>
2021-03-20 20:39:45 -0400

    add imports add dispatch export fetch products

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,10 +1,15 @@
-import {START_LOADING_PRODUCTS, SUCCESS_LOADING_PRODUCTS} from "./index"
+import {
+  START_LOADING_PRODUCTS,
+  START_LOADING_PRODUCT,
+  SUCCESS_LOADING_PRODUCTS,
+  SUCCESS_LOADING_PRODUCT_CATEGORIES
+} from "./index";
 
-
+//this fetch for index container...dbl check it's for the right file!
 export const fetchProducts = () => {
 
   return (dispatch) => {
-    dispatch({type: START_LOADING_PRODUCTS})
+    dispatch({ type: START_LOADING_PRODUCTS })
     
     fetch("http://localhost:3001/products", {
       method: "get",
@@ -13,12 +18,31 @@ export const fetchProducts = () => {
         "Content-Type": "application/json"
       }
     })
+      
       .then((res) => res.json())
       .then((productsJson) => {
         dispatch({
           type: SUCCESS_LOADING_PRODUCTS,
-          payload: productsJson})
+          payload: productsJson
+        })
         
       });
   }
 }
+
+
+export const fetchProducts = (productId) => {
+  return (dispatch) => {
+    dispatch({ type: START_LOADING_PRODUCT, payload: productId })
+  
+    fetch(`http://localhost:3001/products/{productId}`)
+      .then((res) => res.json())
+      .then(({ productCategoriesJson }) => {
+        dispatch({
+          type: SUCCESS_LOADING_PRODUCT_CATEGORIES,
+          payload: productCategoriesJson
+        })
+        
+      });
+  };
+};
