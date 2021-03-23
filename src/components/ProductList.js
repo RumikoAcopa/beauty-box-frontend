086661b954ff12f6e610c573d1ec6ef7c40b0516
rumikoacopa <rumikorios@gmail.com>
2021-03-23 01:02:56 -0400

    remove commented out code, add import, add iteration .map()

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,74 +1,17 @@
-// import React from "react";
-// import {link} from "./react-router-dom";
-
-// const ProductList = ({ products }) => {
-//   return (
-//     <>
-//       <h1>ProductsList</h1>
-//       <div>
-        
-//         {products.map((product) => (
-//           <ProductList key={product.id} product={product.name} />
-          
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default ProductList;
-
 import React from "react";
-import { Link } from "react-router-dom";
+import ListedProducts from "../components/ListedProducts";
 
-const ProductList = ({ product }) => {
+const ProductList = ({ products }) => {
   return (
-    <li className='' key={product.id}>
-      <Link to={`/products/${product.id}`}>{product.name}</Link>
-    </li>
+    <>
+      <h1 className='text-3xl'>Product List</h1>
+      <ul>
+        {products.map((product) => (
+          <ListedProducts key={product.id} product={product} />
+        ))}
+      </ul>
+    </>
   );
 };
 
 export default ProductList;
-
-// import React from 'react';
-// import ProductDetail from './ProductDetail'
-
-// function ProductList(props) {
-
-//   const { products, stockInventory } = props
-
-//   state = {
-//     show: false
-//   }
-
-//   // componentDidMount() {
-
-//   // }
-
-//     return (
-//       <div className="ProductList">
-//         {products.map(product => {
-//           const { id } = product
-
-//           const relevantInventory = stockInventory.filter(se => se.product.id === product.id)
-
-//           const stockTotal = relevantInventory.reduce((accumulator, currentElement) => {
-//             return accumulator + currentElement.quantity
-//           }, 0)
-
-//           return (
-//             <div className="ProductList_ProductsContainer">
-//               <ProductDetail
-//                 name={product.name}
-//                 total={stockTotal}
-//                 stockInventory={relevantInventory}
-//               />
-//             </div>
-//           )
-//         })}
-//       </div>
-//     )
-
-// }
-// export default ProductList;
