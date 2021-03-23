be6d246c4e8758da57d1e3887af8b7f864611063
rumikoacopa <rumikorios@gmail.com>
2021-03-23 01:04:34 -0400

    remove imports, syntax change on fetch, .then change

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,19 +1,18 @@
 import {
   START_LOADING_PRODUCTS,
-  START_LOADING_PRODUCT,
   SUCCESS_LOADING_PRODUCTS,
-  SUCCESS_LOADING_PRODUCT_CATEGORIES
+  SUCCESS_LOADING_PRODUCT_CATEGORIES,
+  SUCCESSFULLY_CREATED_PRODUCT,
 } from "./index";
 
 
 
-//this fetch for index container...dbl check it's for the right file!
 export const fetchProducts = () => {
 
   return (dispatch) => {
     dispatch({ type: START_LOADING_PRODUCTS })
     
-    fetch("http://localhost:3001/products", {
+    fetch(`http://localhost:3001/products`, {
       method: "get",
       headers: {
         Accept: "application/json",
@@ -35,16 +34,45 @@ export const fetchProducts = () => {
 
 export const fetchProductId = (productId) => {
   return (dispatch) => {
-    dispatch({ type: START_LOADING_PRODUCT, payload: productId })
+    dispatch({ type: START_LOADING_PRODUCTS, payload: productId })
   
     fetch(`http://localhost:3001/products/${productId}`)
       .then((res) => res.json())
-      .then(({ productCategoriesJson }) => {
+      .then(( productJson ) => {
         dispatch({
           type: SUCCESS_LOADING_PRODUCT_CATEGORIES,
-          payload: productCategoriesJson
+          payload: productJson
         })
         
       });
   };
 };
+
+export const createProduct = (formData) => {
+  return (dispatch) => {
+    return fetch('http://localhost:3001/products', {
+      method: 'POST',
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product: formData })
+    })
+        .then(res => {
+          if (res.ok) {
+            return
+            res.json()
+          } else {
+            return res.json().then(errors => Promise.reject(errors))
+          }
+        })
+
+        .then(productJson => {
+          dispatch({
+            type: SUCCESSFULLY_CREATED_PRODUCT,
+            payload: productJson,
+          });
+        })
+    }   
+  }
+
