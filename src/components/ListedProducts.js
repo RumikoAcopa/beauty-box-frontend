aba413dca33c8ab06eeabbdd5eba5c7194ab933e
rumikoacopa <rumikorios@gmail.com>
2021-03-23 01:03:36 -0400

    import Link react-router, add link to interpolating product.id/name

--- src/components/ListedProducts.js
+++ src/components/ListedProducts.js
@@ -1,11 +1,15 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ListedProducts = ({ product }) => {
   return (
-    <li className='' key={product.id}>
-      {product.name}
+    <li className='Listed_Products' key={product.id}>
+       <Link to={`/products/${product.id}`}>name {product.name} </Link>
     </li>
   );
 };
 
 export default ListedProducts;
+
+
+    
