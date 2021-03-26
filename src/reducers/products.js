6e5faf627dbde7fbb171e8aeab2ea81a6f6ab61a
rumikoacopa <rumikorios@gmail.com>
2021-03-26 11:06:01 -0400

    add import, product: {}, case and remove

--- src/reducers/products.js
+++ src/reducers/products.js
@@ -1,26 +1,49 @@
 import {
   START_LOADING_PRODUCTS,
+  SUCCESS_LOADING_PRODUCT,
   SUCCESS_LOADING_PRODUCTS,
-  SUCCESS_LOADING_PRODUCT_CATEGORIES,
   SUCCESSFULLY_CREATED_PRODUCT,
+  SUCCESS_LOADING_PRODUCT_CATEGORIES
 } from "../actions";
 
 const defaultState = {
   loadingState: "notStarted",
   list: [],
+  product: {}
 };
+
 
 export default function productsReducer(state = defaultState, action) {
 
   switch (action.type) {
-
     case START_LOADING_PRODUCTS:
       return { ...state, loadingState: "inProgress" };
 
-    case SUCCESS_LOADING_PRODUCTS:
+    case SUCCESS_LOADING_PRODUCT:
       return {
-        list: action.payload,
-        productsloadingState: "successful"
+        ...state,
+        loadingState: false,
+        list: [
+          ...state.list
+            .filter(
+              (product) => product.id !== action.payload.id
+            )
+            .concat(action.payload)
+        ]
+      };
+
+    case SUCCESS_LOADING_PRODUCTS:
+      const products = action.payload;
+      return {
+        ...state,
+        loadingState: false,
+        list: products
+      };
+
+    case SUCCESSFULLY_CREATED_PRODUCT:
+      return {
+        ...state,
+        list: [...state.list.concat(action.payload)]
       };
 
     case SUCCESS_LOADING_PRODUCT_CATEGORIES:
@@ -37,12 +60,6 @@ export default function productsReducer(state = defaultState, action) {
           productsloadingState: "successful"
         };
       }
-
-    case SUCCESSFULLY_CREATED_PRODUCT:
-      return {
-        ...state,
-        list: state.list.concat(action.payload),
-      };
 
     default:
       return state;
