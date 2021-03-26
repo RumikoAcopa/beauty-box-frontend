535037ae2f37a0476b33d72e32d6f0860364edc8
rumikoacopa <rumikorios@gmail.com>
2021-03-26 11:20:52 -0400

    add Footer Component w/img src

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+
+    <div className='empty-container'>
+      <img src="https://images.unsplash.com/photo-1483137140003-ae073b395549?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1000&amp;q=80" alt="green vegetable beside ceramic bowl" width="2000" height="10" />
+    </div>
+  );
+}
+
+export default Footer;
