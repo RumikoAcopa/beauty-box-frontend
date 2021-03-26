e124a14b88cfeb2f95b57766acd4e5d6ed4a7878
rumikoacopa <rumikorios@gmail.com>
2021-03-26 11:21:55 -0400

    modify imports, fetchproducts, fetchproduct & createproduct fetch

--- src/actions/products.js
+++ src/actions/products.js
@@ -1,46 +1,35 @@
-import {
-  START_LOADING_PRODUCTS,
-  SUCCESS_LOADING_PRODUCTS,
-  SUCCESS_LOADING_PRODUCT_CATEGORIES,
-  SUCCESSFULLY_CREATED_PRODUCT,
-} from "./index";
+import { START_LOADING_PRODUCTS, SUCCESS_LOADING_PRODUCTS} from "./index";
+import{START_LOADING_PRODUCT, SUCCESS_LOADING_PRODUCT} from "./index";
+import{SUCCESSFULLY_CREATED_PRODUCT} from "./index";
+import { SUCCESS_LOADING_PRODUCT_CATEGORIES} from "./index";
+
 
 
 
 export const fetchProducts = () => {
-
   return (dispatch) => {
     dispatch({ type: START_LOADING_PRODUCTS })
-    
-    fetch(`http://localhost:3001/products`, {
-      method: "get",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      }
-    })
-      
+    fetch(`http://localhost:3001/products`)   
       .then((res) => res.json())
       .then((productsJson) => {
         dispatch({
           type: SUCCESS_LOADING_PRODUCTS,
           payload: productsJson
-        })
-        
-      });
-  }
-}
+        });
+      })
+  };
+};
 
 
-export const fetchProductId = (productId) => {
+
+export const fetchProduct = (product) => {
   return (dispatch) => {
-    dispatch({ type: START_LOADING_PRODUCTS, payload: productId })
-  
-    fetch(`http://localhost:3001/products/${productId}`)
+    dispatch({ type: START_LOADING_PRODUCT, payload: product})
+    fetch(`http://localhost:3001/products/${product}`)
       .then((res) => res.json())
-      .then(( productJson ) => {
+      .then((productJson) => {
         dispatch({
-          type: SUCCESS_LOADING_PRODUCT_CATEGORIES,
+          type: SUCCESS_LOADING_PRODUCT,
           payload: productJson
         })
         
@@ -50,28 +39,27 @@ export const fetchProductId = (productId) => {
 
 export const createProduct = (formData) => {
   return (dispatch) => {
-    return fetch('http://localhost:3001/products/new', {
+    return fetch("http://localhost:3001/products", {
       method: 'POST',
       headers: {
-        "Accept": "application/json",
+        Accept: "application/json",
         "Content-Type": "application/json",
       },
       body: JSON.stringify({ product: formData })
-    })
-        .then(res => {
-          if (res.ok) {
-            return res.json()
-          } else {
-            return res.json().then(errors => Promise.reject(errors))
-          }
-        })
-
-        .then(productJson => {
-          dispatch({
-            type: SUCCESSFULLY_CREATED_PRODUCT,
-            payload: productJson,
-          });
-        })
-    }   
+      })
+      .then(res => {
+        if (res.ok) {
+          return res.json()
+        } else {
+          return res.json().then(errors => Promise.reject(errors))
+        }
+      })
+      .then(productJson => {
+        dispatch({
+          type: SUCCESSFULLY_CREATED_PRODUCT,
+          payload: productJson,
+        });
+      })
+       
   }
-
+};
